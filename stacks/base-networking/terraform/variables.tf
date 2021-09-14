ccd70450c6b6797d79e7f4cfb4f61f6bb117eb01
Jake Newman <jacob.newman1@nhs.net>
2021-09-14 17:19:26 +0100

    PRMT-2341 [JN/JF] Add VPC config and environment json configuration

---
+++ stacks/base-networking/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  type        = string
+  description = "Uniquely identities each deployment, i.e. dev, prod."
+}
+
+variable "team" {
+  type        = string
+  default     = "Registrations"
+  description = "Team owning this resource"
+}
+
+variable "repo_name" {
+  type        = string
+  default     = "prm-gp-registrations-mi-infra"
+  description = "Name of this repository"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block to assign VPC"
+}
+
+variable "private_cidr_offset" {
+  type        = number
+  description = "CIDR address offset to begin creating private subnets at"
+  default     = 100
+}

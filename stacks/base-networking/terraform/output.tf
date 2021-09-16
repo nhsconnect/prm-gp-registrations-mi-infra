b4af87879bffe447f77c166e49f4c5bd94e1dd5e
Jake Newman <jacob.newman1@nhs.net>
2021-09-16 11:31:33 +0100

    PRMT-2341 [JN/JF/KV] Add private_subnet_id to ssm parameter

---
+++ stacks/base-networking/terraform/output.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "private_subnet_id" {
+  name = "/registrations/${var.environment}/gp-registrations-mi/base-networking/private-subnet-id"
+  type = "String"
+  value = aws_subnet.private.id
+  tags = local.common_tags
+}

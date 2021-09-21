2d424ed29d81bf5bf1154403f65a8f6fb71b1966
Dorota-TW <dorota.sobkow1@nhs.net>
2021-09-21 14:44:33 +0100

    PRMT-2341 [JF/DS] Add a wildcard to public subnet in load balancer

--- stacks/base-networking/terraform/load-balancer.tf
+++ stacks/base-networking/terraform/load-balancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.inbound_only.id]
-  subnets            = [aws_subnet.public.id]
+  subnets            = aws_subnet.public.*.id
 
   enable_deletion_protection = true
 

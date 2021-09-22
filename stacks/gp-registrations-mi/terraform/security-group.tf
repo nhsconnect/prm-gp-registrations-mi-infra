c250b85afd1f25ec31ed4be3b94d210a642f9b33
j-forsb <jade.forsberg@thoughtworks.com>
2021-09-22 11:13:07 +0100

    PRMT-2341 [JF/KV] add outbound security group for alb

--- stacks/gp-registrations-mi/terraform/security-group.tf
+++ stacks/gp-registrations-mi/terraform/security-group.tf
@@ -10,6 +10,19 @@ resource "aws_security_group" "inbound_only" {
   )
 }
 
+resource "aws_security_group" "outbound_only" {
+  name   = "${var.environment}-outbound-only"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-gp-registrations-mi-outbound-only"
+    }
+  )
+}
+
+
 resource "aws_security_group_rule" "inbound_only" {
   type              = "ingress"
   security_group_id = aws_security_group.inbound_only.id
@@ -18,4 +31,14 @@ resource "aws_security_group_rule" "inbound_only" {
   to_port           = 80
   protocol          = "tcp"
   description       = "TLS from VPC"
+}
+
+resource "aws_security_group_rule" "outbound_only" {
+  type              = "egress"
+  security_group_id = aws_security_group.outbound_only.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  description       = "Allow all outbound"
 }

01a3fbd401907efe27c43f31be643d3477396c87
j-forsb <jade.forsberg@thoughtworks.com>
2021-09-23 17:10:40 +0100

    PRMT-2341 [JF] change load balancer listener resource type also change port to 8080 and disable deletion protection for now

--- stacks/gp-registrations-mi/terraform/load-balancer.tf
+++ stacks/gp-registrations-mi/terraform/load-balancer.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "alb" {
   security_groups    = [aws_security_group.mi_alb.id]
   subnets            = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = merge(
     local.common_tags,
@@ -29,12 +29,12 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "alb" {
   name     = "${var.environment}-gp-registrations-mi-alb-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
 }
 
-resource "aws_alb_listener" "http_listener" {
+resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.alb.id
   port              = 80
   protocol          = "HTTP"

c7579e11ea7798143adbb98e9e833a62ed8134fe
helen.zhou <helen.zhou1@nhs.net>
2021-10-21 12:00:41 +0100

    PRMT-2386 [HZ/JN] Add OUTPUT_MI_EVENTS_BUCKET environment variable to GP Registrations MI Task Definition

--- stacks/gp-registrations-mi/terraform/task-definition.tf
+++ stacks/gp-registrations-mi/terraform/task-definition.tf
@@ -25,6 +25,9 @@ resource "aws_ecs_task_definition" "gp_registrations_mi" {
       name      = local.container_name
       image     = "${data.aws_ssm_parameter.gp_registrations_mi_repository_url.value}:${var.gp_registrations_mi_image_tag}"
       essential = true
+      environment = [
+        {"name": "OUTPUT_MI_EVENTS_BUCKET", "value": aws_s3_bucket.mi_output.bucket},
+      ]
       portMappings = [{
         containerPort = 8080
         hostPort      = 8080

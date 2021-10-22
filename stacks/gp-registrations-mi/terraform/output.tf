0ae834a799c95e0572bd7b834f5003ede0d4572f
helen.zhou <helen.zhou1@nhs.net>
2021-10-22 11:49:54 +0100

    PRMT-2386 [HZ] Rename SSM parameter to output MI bucket ARN

--- stacks/gp-registrations-mi/terraform/output.tf
+++ stacks/gp-registrations-mi/terraform/output.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "mi_output_bucket_arn" {
-  name  = "/registrations/${var.environment}/gp-registrations-mi/mi-output-bucket-name"
+  name  = "/registrations/${var.environment}/gp-registrations-mi/mi-output-bucket-arn"
   type  = "String"
   value = aws_s3_bucket.mi_output.arn
   tags  = local.common_tags

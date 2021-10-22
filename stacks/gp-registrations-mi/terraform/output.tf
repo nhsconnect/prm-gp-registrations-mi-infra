3faec9369b19cf0a0df87cee061b05faa70ba7d2
helen.zhou <helen.zhou1@nhs.net>
2021-10-22 17:27:29 +0100

    PRMT-2386 [HZ/AB] Remove MI output bucket ARN from SSM parameters

--- stacks/gp-registrations-mi/terraform/output.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ssm_parameter" "mi_output_bucket_arn" {
-  name  = "/registrations/${var.environment}/gp-registrations-mi/mi-output-bucket-arn"
-  type  = "String"
-  value = aws_s3_bucket.mi_output.arn
-  tags  = local.common_tags
-}

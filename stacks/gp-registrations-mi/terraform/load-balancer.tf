9640157f7060aca4db3a8d878f98933ca30979e2
helen.zhou <helen.zhou1@nhs.net>
2021-10-28 16:06:10 +0100

    PRMT-2396 [HZ/DS/AB] Add Network Load Balancer with target group

--- stacks/gp-registrations-mi/terraform/load-balancer.tf
+++ stacks/gp-registrations-mi/terraform/load-balancer.tf
@@ -48,3 +48,41 @@ resource "aws_lb_listener" "http_listener" {
     type             = "forward"
   }
 }
+
+resource "aws_lb" "nlb" {
+  name               = "${var.environment}-gp-registrations-mi-nlb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-gp-registrations-mi-nlb"
+    }
+  )
+}
+
+resource "aws_lb_target_group" "nlb" {
+  depends_on = [
+    aws_lb.nlb
+  ]
+  name        = "${var.environment}-gp-registrations-mi-nlb-tg"
+  port        = 8080
+  protocol    = "TCP"
+  target_type = "ip"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+  health_check {
+    path = "/actuator/health"
+  }
+}
+
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.id
+  port              = 80
+  protocol          = "TCP"
+  default_action {
+    target_group_arn = aws_lb_target_group.nlb.id
+    type             = "forward"
+  }
+}

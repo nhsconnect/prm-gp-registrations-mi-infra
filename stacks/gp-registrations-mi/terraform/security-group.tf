4268e397ed0af560e18c305204cdfadeca455f79
Elena Alina Balusescu <eabalusescu@ip-192-168-1-207.eu-west-2.compute.internal>
2021-11-04 17:26:49 +0000

    PRMT-2396 [HZ/AB] Remove terraform for ALB setup

--- stacks/gp-registrations-mi/terraform/security-group.tf
+++ stacks/gp-registrations-mi/terraform/security-group.tf
@@ -2,37 +2,6 @@ data "aws_ssm_parameter" "apigee_ips" {
   name = var.apigee_ips_param_name
 }
 
-resource "aws_security_group" "mi_alb" {
-  name   = "${var.environment}-mi-alb"
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.environment}-gp-registrations-mi-alb"
-    }
-  )
-}
-
-resource "aws_security_group_rule" "alb_apigee_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.mi_alb.id
-  cidr_blocks       = split(",", data.aws_ssm_parameter.apigee_ips.value)
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  description       = "Allow inbound traffic from apigee"
-}
-
-resource "aws_security_group_rule" "alb_outbound" {
-  type                     = "egress"
-  security_group_id        = aws_security_group.mi_alb.id
-  source_security_group_id = aws_security_group.gp_registrations_mi_container.id
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  description              = "Allow alb to talk to the container"
-}
-
 resource "aws_security_group" "gp_registrations_mi_container" {
   name   = "${var.environment}-gp-registrations-mi-container"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
@@ -42,16 +11,6 @@ resource "aws_security_group" "gp_registrations_mi_container" {
       Name = "${var.environment}-gp-registrations-mi-container"
     }
   )
-}
-
-resource "aws_security_group_rule" "mi_container_inbound" {
-  type                     = "ingress"
-  security_group_id        = aws_security_group.gp_registrations_mi_container.id
-  source_security_group_id = aws_security_group.mi_alb.id
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  description              = "Allow inbound traffic from load balancer"
 }
 
 resource "aws_security_group_rule" "mi_container_inbound_vpc" {

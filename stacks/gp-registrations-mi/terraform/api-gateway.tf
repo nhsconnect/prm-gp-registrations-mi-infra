3f073e889a2549ef577018794ae878458852aee6
helen.zhou <helen.zhou1@nhs.net>
2021-11-05 15:13:41 +0000

    PRMT-2396 [HZ] Remove API Gateway Account level Cloudwatch IAM roles (to base-infra repo)

--- stacks/gp-registrations-mi/terraform/api-gateway.tf
+++ stacks/gp-registrations-mi/terraform/api-gateway.tf
@@ -59,7 +59,6 @@ resource "aws_api_gateway_deployment" "api_gateway_deployment" {
     aws_api_gateway_integration.api_gateway_integration,
     aws_cloudwatch_log_group.execution_logs,
     aws_cloudwatch_log_group.access_logs,
-    aws_api_gateway_account.api_gateway,
     aws_api_gateway_method.method
   ]
 
@@ -142,52 +141,4 @@ resource "aws_cloudwatch_log_group" "execution_logs" {
       Name = "${var.environment}-gp-registrations-mi"
     }
   )
-}
-
-resource "aws_api_gateway_account" "api_gateway" {
-  cloudwatch_role_arn = aws_iam_role.cloudwatch_role.arn
-}
-
-
-data "aws_iam_policy_document" "assume_iam_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "apigateway.amazonaws.com"
-      ]
-    }
-  }
-}
-
-resource "aws_iam_role" "cloudwatch_role" {
-  name               = "${var.environment}-gp-registrations-mi-api-gateway"
-  description        = "API gateway role to allow for cloudwatch logging"
-  assume_role_policy = data.aws_iam_policy_document.assume_iam_role.json
-}
-
-data "aws_iam_policy_document" "cloudwatch_iam_policy" {
-  statement {
-    sid = "CloudwatchLogs"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams",
-      "logs:PutLogEvents",
-      "logs:GetLogEvents",
-      "logs:FilterLogEvents"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "cloudwatch_role_policy" {
-  name   = "${var.environment}-api-gateway-cloudwatch-log"
-  role   = aws_iam_role.cloudwatch_role.id
-  policy = data.aws_iam_policy_document.cloudwatch_iam_policy.json
 }

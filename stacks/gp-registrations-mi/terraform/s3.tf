1b9a812f4436e10e2209cba7a220251039246f8b
Jake Newman <jacob.newman1@nhs.net>
2022-05-18 14:57:10 +0100

    PRMT-2649 [JN] Adds versioning to S3 bucket files

--- stacks/gp-registrations-mi/terraform/s3.tf
+++ stacks/gp-registrations-mi/terraform/s3.tf
@@ -2,12 +2,28 @@ resource "aws_s3_bucket" "mi_output" {
   bucket = "prm-gp-registrations-mi-events-${var.environment}"
   acl    = "private"
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = merge(
     local.common_tags,
     {
       Name = "Output from GP Registrations MI API"
     }
   )
+}
+
+resource "aws_s3_bucket_acl" "mi_output" {
+  bucket = aws_s3_bucket.mi_output.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "mi_output" {
+  bucket = aws_s3_bucket.mi_output.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "mi_output" {

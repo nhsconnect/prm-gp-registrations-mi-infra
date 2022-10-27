0f98b0ccf9117d921e81c3180bcbf3c704432b0c
Jake Newman <jacob.newman1@nhs.net>
2022-10-27 15:36:00 +0100

    PRMT-2914 [JN] Connect incoming mi events sns topic with queue using subscription

--- stacks/gp-registrations-mi/terraform/iam.tf
+++ stacks/gp-registrations-mi/terraform/iam.tf
@@ -81,3 +81,29 @@ data "aws_iam_policy_document" "apigee_ip_policy" {
 data "aws_ssm_parameter" "apigee_ips" {
   name = var.apigee_ips_param_name
 }
+
+data "aws_iam_policy_document" "sqs_queue_incoming_enriched_mi_events" {
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    resources = [
+      aws_sqs_queue.incoming_enriched_mi_events_for_s3_uploader.arn
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_sns_topic.enriched_mi_events.arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}

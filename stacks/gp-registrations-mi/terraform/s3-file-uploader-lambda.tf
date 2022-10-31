5461c35d2b629bfaf4fcf5057526dfc325037d34
Jake Newman <jacob.newman1@nhs.net>
2022-10-31 14:41:37 +0000

    PRMT-2914 [JN] Setup s3 event uploader lambda trigger from queue

--- stacks/gp-registrations-mi/terraform/s3-file-uploader-lambda.tf
+++ stacks/gp-registrations-mi/terraform/s3-file-uploader-lambda.tf
@@ -12,3 +12,8 @@ resource "aws_lambda_function" "s3_event_uploader_lambda" {
   timeout = 15
   tags          = local.common_tags
 }
+
+resource "aws_lambda_event_source_mapping" "sqs_queue_s3_event_uploader_lambda_trigger" {
+  event_source_arn = aws_sqs_queue.incoming_enriched_mi_events_for_s3_event_uploader.arn
+  function_name    = aws_lambda_function.s3_event_uploader_lambda.arn
+}

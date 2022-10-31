618b4ef2030c0fb5150d80630a6e4958e5e42082
Jake Newman <jacob.newman1@nhs.net>
2022-10-31 16:31:21 +0000

    PRMT-2914 [JN] Try switching order of managed_policy_arns for s3_event_uploader_lambda_role to prevent cycle error of logs

--- stacks/gp-registrations-mi/terraform/iam.tf
+++ stacks/gp-registrations-mi/terraform/iam.tf
@@ -126,14 +126,6 @@ data "aws_iam_policy_document" "incoming_enriched_mi_events_sns_topic" {
   }
 }
 
-resource "aws_iam_role" "s3_event_uploader_lambda_role" {
-  name               = "${var.environment}-s3-event-uploader-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.s3_event_uploader_lambda_assume_role.json
-  managed_policy_arns = [
-    aws_iam_policy.sqs_receive_incoming_enriched_mi_events_for_lambda.arn,
-  ]
-}
-
 resource "aws_cloudwatch_log_group" "s3_event_uploader_lambda" {
   name = "/aws/lambda/${aws_lambda_function.s3_event_uploader_lambda.function_name}"
   tags = merge(
@@ -168,9 +160,13 @@ resource "aws_iam_policy" "incoming_enriched_mi_events_sns_topic_publish" {
   policy = data.aws_iam_policy_document.incoming_enriched_mi_events_sns_topic.json
 }
 
-resource "aws_iam_role_policy_attachment" "s3_event_uploader_lambda_role" {
-  role       = aws_iam_role.s3_event_uploader_lambda_role.name
-  policy_arn = aws_iam_policy.s3_event_uploader_lambda_cloudwatch_log_access.arn
+resource "aws_iam_role" "s3_event_uploader_lambda_role" {
+  name               = "${var.environment}-s3-event-uploader-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.s3_event_uploader_lambda_assume_role.json
+  managed_policy_arns = [
+    aws_iam_policy.s3_event_uploader_lambda_cloudwatch_log_access.arn,
+    aws_iam_policy.sqs_receive_incoming_enriched_mi_events_for_lambda.arn,
+  ]
 }
 
 data "aws_iam_policy_document" "s3_event_uploader_lambda_assume_role" {

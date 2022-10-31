d1fee5ee993acf9d637c1169ac6a0ac31d10281a
Jake Newman <jacob.newman1@nhs.net>
2022-10-31 15:36:38 +0000

    PRMT-2914 [JN] Add cloudwatch access to s3_event_uploader_lambda_role to be able to put log events

--- stacks/gp-registrations-mi/terraform/iam.tf
+++ stacks/gp-registrations-mi/terraform/iam.tf
@@ -136,6 +136,7 @@ resource "aws_iam_role" "s3_event_uploader_lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.s3_event_uploader_lambda_assume_role.json
   managed_policy_arns = [
     aws_iam_policy.sqs_receive_incoming_enriched_mi_events_for_lambda.arn,
+    aws_iam_policy.s3_event_uploader_lambda_cloudwatch_log_access.arn
   ]
 }
 

dbb2929ecad61655594ca3029e5026544c9417ea
Jake Newman <jacob.newman1@nhs.net>
2022-11-04 10:57:26 +0000

    PRMT-2914 [JN] Add default encryption to sns_topic

--- stacks/gp-registrations-mi/terraform/topic.tf
+++ stacks/gp-registrations-mi/terraform/topic.tf
@@ -4,6 +4,7 @@ variable "enriched_mi_events_sns_topic_name" {
 
 resource "aws_sns_topic" "enriched_mi_events" {
   name = "${var.environment}-${var.enriched_mi_events_sns_topic_name}"
+  kms_master_key_id = "alias/aws/sns"
   sqs_failure_feedback_role_arn = aws_iam_role.sns_topic_enriched_mi_events_cloudwatch_log_access_role.arn
   sqs_success_feedback_role_arn = aws_iam_role.sns_topic_enriched_mi_events_cloudwatch_log_access_role.arn
 

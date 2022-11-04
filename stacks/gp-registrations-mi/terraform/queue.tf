371e7b6037c2fed86a1c0577c6d7c8bba687a622
Jake Newman <jacob.newman1@nhs.net>
2022-11-04 16:13:55 +0000

    PRMT-2914 [JN] Create SQS queue for sending raw events that will be enriched with a lambda

--- stacks/gp-registrations-mi/terraform/queue.tf
+++ stacks/gp-registrations-mi/terraform/queue.tf
@@ -5,7 +5,7 @@ resource "aws_sqs_queue" "incoming_mi_events_for_splunk_cloud_event_uploader" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.environment}-gp-registrations-mi-sqs-queue"
+      Name = "${var.environment}-gp-registrations-mi-sqs-queue-for-splunk-cloud"
     }
   )
 }
@@ -15,4 +15,16 @@ resource "aws_sns_topic_subscription" "incoming_mi_events_for_splunk_cloud_event
   raw_message_delivery = true
   topic_arn            = aws_sns_topic.mi_events.arn
   endpoint             = aws_sqs_queue.incoming_mi_events_for_splunk_cloud_event_uploader.arn
+}
+
+resource "aws_sqs_queue" "incoming_mi_events_for_event_enrichment_lambda" {
+  name = "${var.environment}-gp-registrations-mi-events-queue-for-enrichment-lambda"
+  sqs_managed_sse_enabled = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-gp-registrations-mi-sqs-queue-for-enrichment"
+    }
+  )
 }

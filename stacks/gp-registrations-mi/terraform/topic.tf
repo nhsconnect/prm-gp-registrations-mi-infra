41821ba64c9b868962ac6db5cecf60c922c6e8a0
Jake Newman <jacob.newman1@nhs.net>
2022-11-04 08:49:03 +0000

    PRMT-2914 [JN] Fix typo using variable value for enriched_mi_events_sns_topic_name

--- stacks/gp-registrations-mi/terraform/topic.tf
+++ stacks/gp-registrations-mi/terraform/topic.tf
@@ -3,7 +3,7 @@ variable "enriched_mi_events_sns_topic_name" {
 }
 
 resource "aws_sns_topic" "enriched_mi_events" {
-  name = "${var.environment}-${enriched_mi_events_sns_topic_name}"
+  name = "${var.environment}-${var.enriched_mi_events_sns_topic_name}"
   sqs_failure_feedback_role_arn = aws_iam_policy.sns_topic_enriched_mi_events_log_access.arn
   sqs_success_feedback_role_arn = aws_iam_policy.sns_topic_enriched_mi_events_log_access.arn
 

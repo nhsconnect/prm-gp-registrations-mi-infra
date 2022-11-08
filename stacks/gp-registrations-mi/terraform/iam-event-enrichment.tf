330050b77012713f0327d809009ddffae719b334
Jake Newman <jacob.newman1@nhs.net>
2022-11-08 11:56:06 +0000

    PRMT-2928 [JN] Add event enrichment cloudwatch logs to monitor lambda

--- stacks/gp-registrations-mi/terraform/iam-event-enrichment.tf
+++ stacks/gp-registrations-mi/terraform/iam-event-enrichment.tf
@@ -1,3 +1,4 @@
+#Lambda
 resource "aws_iam_role" "event_enrichment_lambda_role" {
   name               = "${var.environment}event-enrichment-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
@@ -6,6 +7,7 @@ resource "aws_iam_role" "event_enrichment_lambda_role" {
   ]
 }
 
+#SQS
 resource "aws_iam_policy" "incoming_mi_events_for_event_enrichment_lambda_sqs_read_access" {
   name   = "${var.environment}-incoming-mi-events-enrichment-lambda-sqs-read"
   policy = data.aws_iam_policy_document.incoming_mi_events_for_event_enrichment_lambda_sqs_read_access.json
@@ -25,3 +27,32 @@ data "aws_iam_policy_document" "incoming_mi_events_for_event_enrichment_lambda_s
   }
 }
 
+#Cloudwatch
+data "aws_iam_policy_document" "event_enrichment_lambda_cloudwatch_log_access" {
+  statement {
+    sid = "CloudwatchLogs"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.event_enrichment_lambda.arn}:*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "event_enrichment_lambda_cloudwatch_log_access" {
+  name   = "${var.environment}-event-enricher-lambda-log-access"
+  policy = data.aws_iam_policy_document.event_enrichment_lambda_cloudwatch_log_access.json
+}
+
+resource "aws_cloudwatch_log_group" "event_enrichment_lambda" {
+  name = "/aws/lambda/${var.environment}-${var.event_enrichment_lambda_name}"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-${var.event_enrichment_lambda_name}"
+    }
+  )
+  retention_in_days = 60
+}

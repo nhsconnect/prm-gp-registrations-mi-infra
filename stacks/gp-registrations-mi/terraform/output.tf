8efa2c8d3927f176eb71fadd83d185145db97967
Jake Newman <jacob.newman1@nhs.net>
2022-11-08 15:29:42 +0000

    PRMT-2928 [JN] Output SQS queue urls to be able to send messages ton

---
+++ stacks/gp-registrations-mi/terraform/output.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "splunk_cloud_event_uploader_sqs_queue_url" {
+  name  = "/registrations/${var.environment}/gp-registrations-mi/splunk-clour-event-uploader-sqs-queue-url"
+  type  = "String"
+  value = aws_sqs_queue.incoming_mi_events_for_splunk_cloud_event_uploader.url
+  tags  = local.common_tags
+}
+
+resource "aws_ssm_parameter" "event_enrichment_sqs_queue_url" {
+  name  = "/registrations/${var.environment}/gp-registrations-mi/event-enrichment-sqs-queue-url"
+  type  = "String"
+  value = aws_sqs_queue.incoming_mi_events_for_event_enrichment_lambda.url
+  tags  = local.common_tags
+}

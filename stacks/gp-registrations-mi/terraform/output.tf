bfff0f9f2cd2b8808ed841e2e2aa6b13099402c5
Jake Newman <jacob.newman1@nhs.net>
2022-11-08 15:39:13 +0000

    PRMT-2928 [JN] Fix output typo

--- stacks/gp-registrations-mi/terraform/output.tf
+++ stacks/gp-registrations-mi/terraform/output.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "splunk_cloud_event_uploader_sqs_queue_url" {
-  name  = "/registrations/${var.environment}/gp-registrations-mi/splunk-clour-event-uploader-sqs-queue-url"
+  name  = "/registrations/${var.environment}/gp-registrations-mi/splunk-cloud-event-uploader-sqs-queue-url"
   type  = "String"
   value = aws_sqs_queue.incoming_mi_events_for_splunk_cloud_event_uploader.url
   tags  = local.common_tags

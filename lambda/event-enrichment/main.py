7cb569197e35837a5e0864cacb41d5cc034ec58d
Jake Newman <jacob.newman1@nhs.net>
2022-11-08 15:44:16 +0000

    PRMT-2928 [JN] Read SPLUNK_CLOUD_EVENT_UPLOADER_SQS_QUEUE_URL from ssm and send message to queue

--- lambda/event-enrichment/main.py
+++ lambda/event-enrichment/main.py
@@ -1,5 +1,5 @@
-import logging
-from http.client import HTTPSConnection
+import os
+import boto3
 
 
 class SsmSecretManager:
@@ -14,13 +14,28 @@ class SsmSecretManager:
 def lambda_handler(sqs_messages, context):
     try:
         print("Enriching event - messages: ", sqs_messages)
-        _enrich_events(sqs_messages)
-        print("Successfully enriched events", sqs_messages)
+        enriched_events = _enrich_events(sqs_messages)
+        print("Successfully enriched events. Preparing to upload", enriched_events)
+        _send_enriched_events_to_sqs_for_uploading(enriched_events)
+        print("Successfully enriched events and sent to SQS event uploader", enriched_events)
         return True
-    except HTTPSConnection as exception:
-        logging.error("Failed to enrich events. " + str(exception))
+    except Exception as exception:
+        print("Failed to enrich events. " + str(exception))
         return False
 
 
 def _enrich_events(sqs_messages):
     print("Finished enriching")
+    return "test"
+
+
+def _send_enriched_events_to_sqs_for_uploading(enriched_events):
+    ssm = boto3.client("ssm")
+    secret_manager = SsmSecretManager(ssm)
+    event_uploader_sqs_queue_url = secret_manager.get_secret(os.environ["SPLUNK_CLOUD_EVENT_UPLOADER_SQS_QUEUE_URL"])
+
+    sqs = boto3.client('sqs')
+    sqs.send_message(
+        QueueUrl=event_uploader_sqs_queue_url,
+        MessageBody=enriched_events
+    )

2290a9ad182f1cb4ba0a9dac044e523b7c8eacce
Jake Newman <jacob.newman1@nhs.net>
2022-11-09 11:30:47 +0000

    PRMT-2928 [JN] Remove sending event to sqs in enrichment lambda as it will happen after success with the destination

--- lambda/event-enrichment/main.py
+++ lambda/event-enrichment/main.py
@@ -1,7 +1,3 @@
-import os
-import boto3
-
-
 class SsmSecretManager:
     def __init__(self, ssm):
         self._ssm = ssm
@@ -16,7 +12,6 @@ def lambda_handler(sqs_messages, context):
         print("Enriching event - messages: ", sqs_messages)
         enriched_events = _enrich_events(sqs_messages)
         print("Successfully enriched events. Preparing to upload", enriched_events)
-        _send_enriched_events_to_sqs_for_uploading(enriched_events)
         print("Successfully enriched events and sent to SQS event uploader", enriched_events)
         return True
     except Exception as exception:
@@ -28,14 +23,3 @@ def _enrich_events(sqs_messages):
     print("Finished enriching")
     return "test"
 
-
-def _send_enriched_events_to_sqs_for_uploading(enriched_events):
-    ssm = boto3.client("ssm")
-    secret_manager = SsmSecretManager(ssm)
-    event_uploader_sqs_queue_url = secret_manager.get_secret(os.environ["SPLUNK_CLOUD_EVENT_UPLOADER_SQS_QUEUE_URL"])
-
-    sqs = boto3.client('sqs')
-    sqs.send_message(
-        QueueUrl=event_uploader_sqs_queue_url,
-        MessageBody=enriched_events
-    )

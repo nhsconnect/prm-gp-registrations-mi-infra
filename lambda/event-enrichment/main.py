da97586bef3b72a54d2a1aeb36ad705e8ac485e4
Jake Newman <jacob.newman1@nhs.net>
2022-11-11 11:34:50 +0000

    PRMT-2928 [JN] Remove aws_lambda_function_event_invoke_config and manually handle success and failure of enrichment (send to SQS queue)

--- lambda/event-enrichment/main.py
+++ lambda/event-enrichment/main.py
@@ -1,4 +1,6 @@
 import json
+import os
+import boto3
 
 
 class SsmSecretManager:
@@ -15,7 +17,8 @@ def lambda_handler(sqs_messages, context):
         print("Enriching events - SQS Records: ", sqs_messages)
         enriched_events = _enrich_events(sqs_messages)
         print("Successfully enriched events:", enriched_events)
-        return enriched_events
+        _send_enriched_events_to_sqs_for_uploading(enriched_events)
+        return True
     except Exception as exception:
         print("Failed to enrich events. " + str(exception))
         return False
@@ -24,3 +27,14 @@ def lambda_handler(sqs_messages, context):
 def _enrich_events(sqs_messages):
     events = sqs_messages["Records"]
     return [json.loads(event["body"]) for event in events]
+
+
+def _send_enriched_events_to_sqs_for_uploading(enriched_events):
+    print("Sending enriched events to SQS for uploading to splunk cloud", enriched_events)
+    event_uploader_sqs_queue_url = os.environ["SPLUNK_CLOUD_EVENT_UPLOADER_SQS_QUEUE_URL"]
+
+    sqs = boto3.client('sqs')
+    sqs.send_message(
+        QueueUrl=event_uploader_sqs_queue_url,
+        MessageBody=enriched_events
+    )

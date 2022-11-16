41722e4244d68c6226ff41a691d6bae8648a555e
Jake Newman <jacob.newman1@nhs.net>
2022-11-16 12:11:31 +0000

    PRMT-2928 [JN] Handle case when ICB is None (ie. no organisation returned from ODS code) and improve logging

--- lambda/event-enrichment/main.py
+++ lambda/event-enrichment/main.py
@@ -48,19 +48,22 @@ def _enrich_events(sqs_messages):
         requesting_practice_organisation = _fetch_organisation(event["requestingPracticeOdsCode"])
         event["requestingPracticeName"] = requesting_practice_organisation["Name"]
         event["requestingPracticeIcbOdsCode"] = _find_icb_ods_code(requesting_practice_organisation)
-        event["requestingPracticeIcbName"] = _fetch_organisation_name(event["requestingPracticeIcbOdsCode"])
+        event["requestingPracticeIcbName"] = _fetch_organisation(event["requestingPracticeIcbOdsCode"])["Name"]
 
-        if event["sendingPracticeOdsCode"]:
-            sending_practice_organisation = _fetch_organisation(event["sendingPracticeOdsCode"])
-            event["sendingPracticeName"] = sending_practice_organisation["Name"]
-            event["sendingPracticeIcbOdsCode"] = _find_icb_ods_code(sending_practice_organisation)
-            event["sendingPracticeIcbName"] = _fetch_organisation_name(event["sendingPracticeIcbOdsCode"])
+        sending_practice_organisation = _fetch_organisation(event["sendingPracticeOdsCode"])
+        event["sendingPracticeName"] = sending_practice_organisation["Name"]
+        event["sendingPracticeIcbOdsCode"] = _find_icb_ods_code(sending_practice_organisation)
+        event["sendingPracticeIcbName"] = _fetch_organisation(event["sendingPracticeIcbOdsCode"])["Name"]
 
     return events
 
 
 def _find_icb_ods_code(practice_organisation):
-    print("Finding icb_ods_code for practice organisation", practice_organisation)
+    print("Finding ICB ODS code for practice organisation", practice_organisation)
+    if not hasattr(practice_organisation, "Rels"):
+        print("No ICB ODS code for practice organisation", practice_organisation)
+        return None
+
     try:
         organisation_relationships = practice_organisation["Rels"]["Rel"]
         print("organisation_relationships", organisation_relationships)
@@ -68,13 +71,13 @@ def _find_icb_ods_code(practice_organisation):
             organisation_details for organisation_details in organisation_relationships if _is_icb(organisation_details))
 
         if organisation_rel_containing_icb_code:
-            print("Found organisation_rel_containing_icb_code", organisation_rel_containing_icb_code)
+            print("Found organisation rel containing ICB ODS code", organisation_rel_containing_icb_code)
             return organisation_rel_containing_icb_code["Target"]["OrgId"]["extension"]
         else:
-            print("No organisation_rel_containing_icb_code found for organisation", practice_organisation)
+            print("No organisation rel containing ICB ODS code for organisation", practice_organisation)
             return None
     except Exception as e:
-        print("Unable to find icb information for practice " + str(practice_organisation), e)
+        print("Unable to find ICB information for practice " + str(practice_organisation), e)
         return None
 
 
@@ -90,20 +93,20 @@ def _fetch_organisation(ods_code: str):
     if ods_code is None:
         return EMPTY_ORGANISATION
 
-    print("Attempting to retrieve organisation with ods_code: " + ods_code)
+    print("Attempting to retrieve organisation with ODS code: " + ods_code)
     http = urllib3.PoolManager()
     response = http.request('GET', ODS_PORTAL_URL + ods_code)
 
     if response.status == 404:
-        print("Unable to find organisation with ods code" + ods_code)
+        print("Unable to find organisation with ODS code" + ods_code)
         return EMPTY_ORGANISATION
 
     if response.status != 200:
         raise OdsPortalException(
-            "Unable to fetch organisation data for ods_code:" + ods_code + ". Response status code: " + str(
+            "Unable to fetch organisation data for ODS code:" + ods_code + ". Response status code: " + str(
                 response.status), response)
 
-    print("Successfully retrieved organisation with ods_code: " + ods_code)
+    print("Successfully retrieved organisation with ODS code: " + ods_code)
     response_content = json.loads(response.data.decode('utf-8'))
     return response_content["Organisation"]
 

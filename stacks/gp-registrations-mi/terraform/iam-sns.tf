1a37041226b2652d472a6d8b4823d31ec4de5450
jowita <jowita.podolak@gmail.com>
2022-11-30 16:23:05 +0000

    PRMT-2998 [JN/JP] Add policy to send message from sns to sqs

--- stacks/gp-registrations-mi/terraform/iam-sns.tf
+++ stacks/gp-registrations-mi/terraform/iam-sns.tf
@@ -15,3 +15,33 @@ data "aws_iam_policy_document" "sns_publish" {
   }
 }
 
+resource "aws_sqs_queue_policy" "incoming_enriched_mi_events_for_s3_uploader" {
+  queue_url = aws_sqs_queue.incoming_mi_events_for_s3_uploader.id
+  policy    = data.aws_iam_policy_document.sqs_queue_incoming_enriched_mi_events.json
+}
+
+data "aws_iam_policy_document" "sqs_queue_incoming_enriched_mi_events" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    resources = [
+      aws_sqs_queue.incoming_mi_events_for_s3_uploader.arn,
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_sns_topic.enriched_events_topic.arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}
+

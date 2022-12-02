ff70da4b373d565586edc6aeb2ce675925608f8b
jowita <jowita.podolak@gmail.com>
2022-12-02 15:56:52 +0000

    PRMT-2998 [JN/JP] Add s3-event-uploader lambda

--- stacks/gp-registrations-mi/terraform/iam-s3-event-uploader.tf
+++ stacks/gp-registrations-mi/terraform/iam-s3-event-uploader.tf
@@ -4,7 +4,8 @@ resource "aws_iam_role" "s3_event_uploader_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 
   managed_policy_arns = [
-    aws_iam_policy.mi_events_output_s3_put_access.arn
+    aws_iam_policy.mi_events_output_s3_put_access.arn,
+    aws_iam_policy.incoming_mi_events_for_s3_event_uploader_lambda_sqs_read_access.arn
   ]
 }
 
@@ -26,6 +27,23 @@ data "aws_iam_policy_document" "mi_events_output_s3_put_access" {
       aws_s3_bucket.mi_events_output.arn
     ]
   }
-
 }
 
+resource "aws_iam_policy" "incoming_mi_events_for_s3_event_uploader_lambda_sqs_read_access" {
+  name   = "${var.environment}-incoming-mi-events-for-s3-event-uploader-lambda-sqs-read-access"
+  policy = data.aws_iam_policy_document.incoming_mi_events_for_splunk_cloud_event_uploader_lambda_sqs_read_access.json
+}
+
+data "aws_iam_policy_document" "incoming_mi_events_for_s3_event_uploader_lambda_sqs_read_access" {
+  statement {
+    actions = [
+      "sqs:GetQueue*",
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.incoming_mi_events_for_s3_event_uploader.arn,
+    ]
+  }
+}

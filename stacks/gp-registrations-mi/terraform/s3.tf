6a4d8595dd2f1d2c77ea596a6fbeae71ecd1c830
Jake Newman <jacob.newman1@nhs.net>
2022-12-02 10:30:43 +0000

    PRMT-2998 [JN/JP] Add aws_s3_bucket_public_access_block to s3 mi output bucket
    Rename mi_events to mi_events_output

--- stacks/gp-registrations-mi/terraform/s3.tf
+++ stacks/gp-registrations-mi/terraform/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "mi_events" {
+resource "aws_s3_bucket" "mi_events_output" {
   bucket = "prm-gp-registrations-mi-events-${var.environment}"
 
   lifecycle {
@@ -13,25 +13,34 @@ resource "aws_s3_bucket" "mi_events" {
   )
 }
 
-resource "aws_s3_bucket_acl" "mi_events" {
-  bucket = aws_s3_bucket.mi_events.id
+resource "aws_s3_bucket_acl" "mi_events_output" {
+  bucket = aws_s3_bucket.mi_events_output.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "mi_events" {
-  bucket = aws_s3_bucket.mi_events.id
+resource "aws_s3_bucket_versioning" "mi_events_output" {
+  bucket = aws_s3_bucket.mi_events_output.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "mi_events" {
-  bucket = aws_s3_bucket.mi_events.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "mi_events_output" {
+  bucket = aws_s3_bucket.mi_events_output.bucket
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "mi_events_output" {
+  bucket = aws_s3_bucket.mi_events_output.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }

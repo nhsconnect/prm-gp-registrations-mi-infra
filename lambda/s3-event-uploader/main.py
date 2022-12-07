0f64354ac69d947cd2e42f328666a5a2cb0c43de
jowita <jowita.podolak@gmail.com>
2022-12-07 09:16:30 +0000

    PRMT-2998 [JP] Fix tests for s3 uploader lambda

--- lambda/s3-event-uploader/main.py
+++ lambda/s3-event-uploader/main.py
@@ -15,29 +15,20 @@ def _upload_events_to_s3(sqs_messages):
     s3_bucket = boto3.client('s3')
     for event in events_list:
         s3_bucket.put_object(Bucket=os.environ["MI_EVENTS_OUTPUT_S3_BUCKET_NAME"], Key=_generate_s3_key(event),
-                             Body=event)
+                             Body=json.dumps(event))
     return True
 
 
 def _extract_events_from_sqs_messages(sqs_messages):
-    print(sqs_messages)
     event_records = sqs_messages["Records"]
-    print(event_records)
-    events_with_message_list = [event_record["body"] for event_record in
-                                event_records]
-    print(events_with_message_list)
-    events = [(json.loads(event_with_message)) for event_with_message in events_with_message_list]  # remove nested list created
-    print(events)
-    print("------"*10)
-    events = [(json.loads(event_with_message["Message"])) for event_with_message in events_with_message_list]  # remove nested list created
-    print(events)
+    event_records_list = [json.loads(event_record["body"]) for event_record in
+                          event_records]
+    events = [(json.loads(event_with_message["Message"])) for event_with_message in event_records_list][0]  # remove nested list created
     return events
 
 
 def _generate_s3_key(event):
-    event_object = json.loads(event)
-    event_id = event_object["eventId"]
-    event_date_time = event_object["registrationEventDateTime"]
-
+    event_id = event["eventId"]
+    event_date_time = event["registrationEventDateTime"]
     s3_path = datetime.datetime.strptime(event_date_time, '%Y-%m-%dT%H:%M:%S%z').strftime('%Y/%m/%d/%H')
     return s3_path + "/" + event_id + ".json"
--- lambda/s3-event-uploader/test_main.py
+++ lambda/s3-event-uploader/test_main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import unittest
 from unittest.mock import patch, MagicMock
@@ -8,9 +9,10 @@ class TestMain(unittest.TestCase):
     @patch('boto3.client')
     @patch.dict(os.environ, {"MI_EVENTS_OUTPUT_S3_BUCKET_NAME": "test_bucket_name"})
     def test_should_upload_events_to_s3(self, mock_boto_client):
-        message_of_event = """{"Message":'[{"eventId": "event_id_1"}]'}"""
+        events = '[{"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}]'
+        message_of_event = json.dumps({"Message":events})
         lambda_input = {"Records": [{"body": message_of_event}]}
-        expected_event = {"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}
+        expected_event = '{"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}'
 
         put_spy = MagicMock()
         mock_boto_client("s3").put_object = put_spy
@@ -20,20 +22,18 @@ class TestMain(unittest.TestCase):
         put_spy.assert_called_once_with(Bucket="test_bucket_name", Key=expected_path, Body=expected_event)
 
     def test_extracting_events_from_sqs_messages(self):
-        lambda_input = {"Records": [{"body": {"Message": [{"eventId": "event_id_1",
-                                                           "registrationEventDateTime": "2000-01-02T09:10:00Z",
-                                                           "eventType": "REGISTRATIONS",
-                                                           "requestingPracticeOdsCode": "ODS_1",
-                                                           "sendingPracticeOdsCode": "ODS_1"}]}}]}
+        events = '[{"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}]'
+        message_of_event = json.dumps({"Message":events})
+        lambda_input = {"Records": [{"body": message_of_event}]}
+
         extracted_event = _extract_events_from_sqs_messages(lambda_input)
 
-        event = {"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z",
-                 "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}
+        event = {"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}
         assert extracted_event == [event]
 
     def test_generate_s3_key(self):
-        event = """{"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType": 
-        "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}"""
+        event = {"eventId": "event_id_1", "registrationEventDateTime": "2000-01-02T09:10:00Z", "eventType":
+        "REGISTRATIONS", "requestingPracticeOdsCode": "ODS_1", "sendingPracticeOdsCode": "ODS_1"}
 
         expected_key = "2000/01/02/09/event_id_1.json"
         result = _generate_s3_key(event)

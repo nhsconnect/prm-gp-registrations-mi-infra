b2d376736c37fc4841ba721da14eb8653781762a
jowita <jowita.podolak1@nhs.net>
2023-01-10 11:12:12 +0000

    PRMT-3027 [JP/JN] Add propagate tags in ecs service

--- stacks/gp-registrations-mi/terraform/ecs-service.tf
+++ stacks/gp-registrations-mi/terraform/ecs-service.tf
@@ -13,6 +13,7 @@ resource "aws_ecs_service" "mi_service" {
   task_definition = aws_ecs_task_definition.gp_registrations_mi.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+  propagate_tags = "SERVICE"
 
   network_configuration {
     subnets         = split(",", data.aws_ssm_parameter.private_subnet_ids.value)

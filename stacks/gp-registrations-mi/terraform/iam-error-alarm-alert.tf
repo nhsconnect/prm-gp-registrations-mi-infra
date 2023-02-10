c90e252681770e95635b085dc0b1398eb1a64735
Jake Newman <jacob.newman1@nhs.net>
2023-02-10 15:53:07 +0000

    PRMT-3100 [JN] Setup ssm read access for error-alarm-alert-lambda to read webhook url to send alerts to

--- stacks/gp-registrations-mi/terraform/iam-error-alarm-alert.tf
+++ stacks/gp-registrations-mi/terraform/iam-error-alarm-alert.tf
@@ -3,8 +3,29 @@ resource "aws_iam_role" "error_alarm_alert_lambda_role" {
   name               = "${var.environment}-error-alarm-alert-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
   managed_policy_arns = [
-    aws_iam_policy.error_alarm_alert_lambda_cloudwatch_log_access.arn
+    aws_iam_policy.error_alarm_alert_lambda_cloudwatch_log_access.arn,
+    aws_iam_policy.error_alarm_alert_lambda_ssm_access.arn,
   ]
+}
+
+#SSM
+resource "aws_iam_policy" "error_alarm_alert_lambda_ssm_access" {
+  name   = "${var.environment}-error-alarm-alert-lambda-ssm-access"
+  policy = data.aws_iam_policy_document.error_alarm_alert_lambda_ssm_access.json
+}
+
+data "aws_iam_policy_document" "error_alarm_alert_lambda_ssm_access" {
+  statement {
+    sid = "GetSSMParameter"
+
+    actions = [
+      "ssm:GetParameter"
+    ]
+
+    resources = [
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.log_alerts_general_webhook_url_param_name}",
+    ]
+  }
 }
 
 #Cloudwatch

58d9fe6be1bdb062ef679aeb54930881996c5ea0
Jake Newman <jacob.newman1@nhs.net>
2023-02-10 15:40:26 +0000

    PRMT-3100 [JN] Fix typo in cloudwatch error-alarm-alert policy access

--- stacks/gp-registrations-mi/terraform/iam-error-alarm-alert.tf
+++ stacks/gp-registrations-mi/terraform/iam-error-alarm-alert.tf
@@ -1,6 +1,6 @@
 #Lambda
 resource "aws_iam_role" "error_alarm_alert_lambda_role" {
-  name               = "${var.environment}-error_alarm_alert-lambda-role"
+  name               = "${var.environment}-error-alarm-alert-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
   managed_policy_arns = [
     aws_iam_policy.error_alarm_alert_lambda_cloudwatch_log_access.arn
@@ -9,7 +9,7 @@ resource "aws_iam_role" "error_alarm_alert_lambda_role" {
 
 #Cloudwatch
 resource "aws_iam_policy" "error_alarm_alert_lambda_cloudwatch_log_access" {
-  name   = "${var.environment}-event-enricher-lambda-log-access"
+  name   = "${var.environment}-error-alarm-alert-lambda-log-access"
   policy = data.aws_iam_policy_document.error_alarm_alert_lambda_cloudwatch_log_access.json
 }
 

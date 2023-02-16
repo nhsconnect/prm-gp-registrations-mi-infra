89516f2a191420d9de7279b190904042de4e28ec
Jake Newman <jacob.newman1@nhs.net>
2023-02-16 11:14:19 +0000

    PRMT-3100 [JN] Add alarm for event-enrichment-lambda-dlq

--- stacks/gp-registrations-mi/terraform/monitoring-alarms.tf
+++ stacks/gp-registrations-mi/terraform/monitoring-alarms.tf
@@ -35,3 +35,23 @@ resource "aws_cloudwatch_metric_alarm" "splunk_cloud_event_uploader_lambda_dlq_a
 
   alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "event_enrichment_lambda_dlq_alarm" {
+  alarm_name          = "${var.environment}-event-enrichment-lambda-dlq-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "0"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.incoming_mi_events_for_event_enrichment_lambda_dlq.name
+  }
+
+  alarm_description = "There is an item in the dlq that must be actioned by replaying the messages, or fixing the underlying issue. See cloudwatch logs for relevant resource for more details"
+
+  alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
+}

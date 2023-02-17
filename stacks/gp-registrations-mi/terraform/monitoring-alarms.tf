511b15d46eb0b281dcb4daee025f1deb634cb4fb
Jake Newman <jacob.newman1@nhs.net>
2023-02-17 11:11:13 +0000

    PRMT-3100 [JN] Add alarms for API Gateway (4xx and 5xx)

--- stacks/gp-registrations-mi/terraform/monitoring-alarms.tf
+++ stacks/gp-registrations-mi/terraform/monitoring-alarms.tf
@@ -70,7 +70,7 @@ resource "aws_cloudwatch_metric_alarm" "enriched_events_sns_failure_alarm" {
     TopicName = aws_sns_topic.enriched_events_topic.name
   }
 
-  alarm_description = "There is an issue with SNS topic: ${aws_sns_topic.enriched_events_topic.name}. See cloudwatch logs for relevant resource for more details."
+  alarm_description = "There is an issue with SNS topic: ${aws_sns_topic.enriched_events_topic.name}. See cloudwatch logs for relevant resource to find more details."
 
   alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
 }
@@ -89,7 +89,7 @@ resource "aws_cloudwatch_metric_alarm" "event_enrichment_lambda_alarm" {
     FunctionName = "${var.environment}-${var.event_enrichment_lambda_name}"
   }
 
-  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource for more details."
+  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource to find more details."
 
   alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
 }
@@ -108,7 +108,7 @@ resource "aws_cloudwatch_metric_alarm" "splunk_cloud_event_uploader_lambda_alarm
     FunctionName = "${var.environment}-${var.splunk_cloud_event_uploader_lambda_name}"
   }
 
-  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource for more details."
+  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource to find more details."
 
   alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
 }
@@ -127,8 +127,47 @@ resource "aws_cloudwatch_metric_alarm" "s3_event_uploader_lambda_alarm" {
     FunctionName = "${var.environment}-${var.s3_event_uploader_lambda_name}"
   }
 
-  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource for more details."
+  alarm_description = "There is an issue with running a lambda. See cloudwatch logs for relevant resource to find more details."
 
   alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
 }
+
+resource "aws_cloudwatch_metric_alarm" "api_gateway_5XX_errors_alarm" {
+  alarm_name          = "${aws_api_gateway_rest_api.rest_api.name}-5XX-errors-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "5XXError"
+  namespace           = "AWS/ApiGateway"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "0"
+
+  dimensions = {
+    ApiName = aws_api_gateway_rest_api.rest_api.name
+  }
+
+  alarm_description = "There is in the api-gateway. See api-gateway to find more details."
+
+  alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_gateway_4XX_errors_alarm" {
+  alarm_name          = "${aws_api_gateway_rest_api.rest_api.name}-4XX-errors-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "4XXError"
+  namespace           = "AWS/ApiGateway"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "0"
+
+  dimensions = {
+    ApiName = aws_api_gateway_rest_api.rest_api.name
+  }
+
+  alarm_description = "There is a 4xx status code error observed in the api-gateway. See cloudwatch logs for relevant resource to find more details."
+
+  alarm_actions = [aws_sns_topic.error_alarm_alert_topic.arn]
+}
+
 

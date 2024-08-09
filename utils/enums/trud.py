cb62c2d89d754950f58eb90263cfe34421162171
abbas-khan10 <abbas.khan10@nhs.net>
2024-08-09 16:50:56 +0100

    Add bulk ods update lambda

---
+++ utils/enums/trud.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+
+
+class TrudItem(StrEnum):
+    NHS_ODS_WEEKLY = "58"
+    ORG_REF_DATA_MONTHLY = "242"
+
+
+class OdsDownloadType(StrEnum):
+    GP = "ODS_GP"
+    ICB = "ODS_ICB"
+    BOTH = "ODS_GP_ICB"

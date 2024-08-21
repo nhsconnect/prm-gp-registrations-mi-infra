74e29e6c5d10025adb33efe0326753a902e9b8cb
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2024-08-21 16:25:05 +0100

    PRMP-0000- Update lambda_layer tf to include source code hash

--- stacks/gp-registrations-mi/terraform/lambda_layer.tf
+++ stacks/gp-registrations-mi/terraform/lambda_layer.tf
@@ -3,4 +3,5 @@ resource "aws_lambda_layer_version" "mi_enrichment" {
   layer_name               = "${var.environment}_mi_enrichment_layer"
   compatible_runtimes      = ["python3.12"]
   compatible_architectures = ["x86_64"]
+  source_code_hash         = filebase64sha256(var.mi_enrichment_lambda_layer_zip)
 }

05bd82f74f008f0702e40f2204c039e37fcc2c72
Steph Torres <stephane.torres1@nhs.net>
2025-07-16 12:44:26 +0100

    [PRM-410] install ruff and format

--- stacks/degrades-reporting/terraform/lambda_layer.tf
+++ stacks/degrades-reporting/terraform/lambda_layer.tf
@@ -4,8 +4,9 @@ resource "aws_lambda_layer_version" "degrades" {
   compatible_architectures = ["x86_64"]
   source_code_hash         = filebase64sha256("${var.degrades_lambda_layer_zip}")
   filename                 = var.degrades_lambda_layer_zip
+}
 
-}resource "aws_lambda_layer_version" "pandas" {
+resource "aws_lambda_layer_version" "pandas" {
   layer_name               = "${var.environment}_degrades_lambda_layer"
   compatible_runtimes      = ["python3.12"]
   compatible_architectures = ["x86_64"]

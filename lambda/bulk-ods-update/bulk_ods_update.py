2c712c9af0ed42c7a8e610345e0dac20652fe4b6
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-09-15 16:23:57 +0100

    Added ODS_BACKFILL_FLAG and DEBUGGING FLAG

--- lambda/bulk-ods-update/bulk_ods_update.py
+++ lambda/bulk-ods-update/bulk_ods_update.py
@@ -19,6 +19,8 @@ from utils.constants.ods_constants import (
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+FULL_BACKFILL = os.getenv("ODS_FULL_BACKFILL_FLAG", "false").lower()
+DEBUG_LOG_RETURN = os.getenv("ODS_DEBUG", "false").lower()
 TEMP_DIR = tempfile.mkdtemp(dir="/tmp")
 seven_days_ago = (datetime.now(timezone.utc) - timedelta(days=7)).strftime("%Y-%m-%d")
 
@@ -37,22 +39,22 @@ def lambda_handler(event, context):
 
 def extract_and_process_ods_gp_data(ods_service: OdsApiService):
     logger.info("Extracting and processing ODS GP data")
+    url = (ods_service.api_url + GP_REPORT_NAME
+            if FULL_BACKFILL
+            else ods_service.api_url + GP_REPORT_NAME + ODS_API_DATE_QUERY + seven_days_ago)
 
-    download_file = ods_service.get_download_file(
-        ods_service.api_url + GP_REPORT_NAME + ODS_API_DATE_QUERY + seven_days_ago
-    )
+    download_file = ods_service.get_download_file(url)
     file_name = os.path.join(TEMP_DIR, GP_FILE_NAME)
     
     with open(file_name, "wb") as f:
         f.write(download_file)
 
     gp_ods_data = ods_csv_to_dict(file_name, GP_FILE_HEADERS)
-    gp_ods_data_amended_data = get_amended_records(gp_ods_data)
+    gp_ods_data_amended_data = gp_ods_data if FULL_BACKFILL else get_amended_records(gp_ods_data)
 
     if gp_ods_data_amended_data:
-        logger.info(
-            f"Found {len(gp_ods_data_amended_data)} amended GP data records to update"
-        )
+        logger.info(f"GP records to update: {len(gp_ods_data_amended_data)} (full_backfill={FULL_BACKFILL})")
+
         compare_and_overwrite(OdsDownloadType.GP, gp_ods_data_amended_data)
         return
 
@@ -61,30 +63,23 @@ def extract_and_process_ods_gp_data(ods_service: OdsApiService):
 
 def extract_and_process_ods_icb_data(ods_service: OdsApiService):
     logger.info("Getting latest ICB release details")
-
-    logger.info("Proceeding to download ICB data")
-    url = ods_service.api_url + ICB_REPORT_NAME + ODS_API_DATE_QUERY + seven_days_ago
+    url = (ods_service.api_url + ICB_REPORT_NAME
+            if FULL_BACKFILL
+            else ods_service.api_url + ICB_REPORT_NAME + ODS_API_DATE_QUERY + seven_days_ago)
 
     download_file = ods_service.get_download_file(url)
-        
-    icb_ods_data_amended_data = []
-    logger.info("Extracting and processing ODS ICB data")
-
     file_name = os.path.join(TEMP_DIR, ICB_FILE_NAME)
     with open(file_name, "wb") as f:
         f.write(download_file)
-    
+
     icb_ods_data = ods_csv_to_dict(file_name, ICB_FILE_HEADERS)
-    icb_ods_data_amended_data = get_amended_records(icb_ods_data)
+    icb_ods_data_amended_data = icb_ods_data if FULL_BACKFILL else get_amended_records(icb_ods_data)
 
     if icb_ods_data_amended_data:
-        logger.info(
-            f"Found {len(icb_ods_data_amended_data)} amended ICB data records to update"
-        )
+        logger.info(f"ICB records to update: {len(icb_ods_data_amended_data)} (full_backfill={FULL_BACKFILL})")
         compare_and_overwrite(OdsDownloadType.ICB, icb_ods_data_amended_data)
-        return
-
-    logger.info("No amended ICB data found")
+    else:
+        logger.info("No ICB data to update")
 
 
 def get_amended_records(data: list[dict]) -> list[dict]:
@@ -106,41 +101,41 @@ def ods_csv_to_dict(file_path: str, headers: list[str]) -> list[dict]:
             data_list.append(dict(row))
         return data_list
 
-
+def update_gp_data(amended_record: dict):
+    try:
+        odsCode = amended_record.get("PracticeOdsCode")
+        practice = PracticeOds(odsCode)
+        res = practice.update(actions=[
+            PracticeOds.practice_name.set(amended_record.get("PracticeName")), 
+            PracticeOds.practice_status.set(status_from_close_date(amended_record.get("CloseDate"))),
+            PracticeOds.last_updated.set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            ], return_values="ALL_NEW" if DEBUG_LOG_RETURN else "NONE")
+        if DEBUG_LOG_RETURN:
+            logger.info(f"Updated Practice {odsCode}: {res.attribute_values}")
+    except Exception as e:
+        logger.info(f"Failed to create/update record by Practice ODS code: {str(e)}")
+        
+def update_icb_data(amended_record: dict):
+    try:
+        odsCode = amended_record.get("IcbOdsCode")
+        icb = IcbOds(odsCode)
+        res = icb.update(actions=[
+            IcbOds.icb_name.set(amended_record.get("IcbName")), 
+            IcbOds.icb_status.set(status_from_close_date(amended_record.get("CloseDate"))),
+            IcbOds.last_updated.set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            ], return_values="ALL_NEW" if DEBUG_LOG_RETURN else "NONE")
+        if DEBUG_LOG_RETURN:
+            logger.info(f"Updated ICB {odsCode}: {res.attribute_values}")
+    except Exception as e:
+        logger.info(f"Failed to create/update record by ICB ODS code: {str(e)}")
+        
 def compare_and_overwrite(download_type: OdsDownloadType, data: list[dict]):
     if download_type == OdsDownloadType.GP:
         logger.info("Comparing GP Practice data")
         for amended_record in data:
-            try:
-                practice = PracticeOds(amended_record.get("PracticeOdsCode"))
-                practice.update(
-                    actions=[
-                        PracticeOds.practice_name.set(
-                            amended_record.get("PracticeName")
-                        ),
-                        PracticeOds.icb_ods_code.set(amended_record.get("IcbOdsCode")),
-                        PracticeOds.practice_status.set(status_from_close_date(amended_record.get("CloseDate"))),
-                        PracticeOds.last_updated.set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-                    ]
-                )
-                logger.info(
-                    f'Overwriting for ODS: {amended_record.get("PracticeOdsCode")} - Name: {amended_record.get("PracticeName")} | ICB: {amended_record.get("IcbOdsCode")}')
-            except Exception as e:
-                logger.info(
-                    f"Failed to create/update record by Practice ODS code: {str(e)}"
-                )
+            update_gp_data(amended_record)
 
     if download_type == OdsDownloadType.ICB:
         logger.info("Comparing ICB data")
         for amended_record in data:
-            try:
-                icb = IcbOds(amended_record.get("IcbOdsCode"))
-                icb.update(actions=[
-                    IcbOds.icb_name.set(amended_record.get("IcbName")), 
-                    IcbOds.icb_status.set(status_from_close_date(amended_record.get("CloseDate"))),
-                    IcbOds.last_updated.set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-                    ])
-                logger.info(
-                    f'Overwriting for ODS: {amended_record.get("IcbOdsCode")} - Name: {amended_record.get("IcbName")}')
-            except Exception as e:
-                logger.info(f"Failed to create/update record by ICB ODS code: {str(e)}")
+            update_icb_data(amended_record)
